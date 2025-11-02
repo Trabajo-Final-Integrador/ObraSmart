cde83b5a933e82c5a598fbac98dd2d4f0847c66b
114320_FERREYRA_FERNANDO_GABRIEL <114320@tecnicatura.frc.utn.edu.ar>
2025-11-02 17:02:37 -0300

    .

---
+++ GestionGeolocalizacion/src/test/java/com/ObraSmart/GestionGeolocalizacion/GestionGeolocalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ObraSmart.GestionGeolocalizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGeolocalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ GestionReportes/src/test/java/com/ObraSmart/GestionReportes/GestionReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ObraSmart.GestionReportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
