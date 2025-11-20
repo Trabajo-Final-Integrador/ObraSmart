64e9c416fd155999497a306d30ad03162458bbf8
114320_FERREYRA_FERNANDO_GABRIEL <114320@tecnicatura.frc.utn.edu.ar>
2025-11-20 19:10:13 -0300

    .COMIT ASISTENTE FINAL ULTA

--- GestionAsistentev2/src/main/java/com/ObraSmart/GestionAsistente/Dto/RequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.ObraSmart.GestionAsistente.Dto;
-
-/**
- * DTO utilizado para recibir un mensaje del usuario
- * desde el frontend o Postman.
- */
-public record RequestDto(String mensaje) {}
--- GestionAsistentev2/src/main/java/com/ObraSmart/GestionAsistente/Service/IA/IaLocalService.java
+++
@@ -1,9 +0,0 @@
-package com.ObraSmart.GestionAsistente.Service.IA;
-
-
-/**
- * Servicio para IA local (Ollama).
- */
-public interface IaLocalService {
-    String responder(String mensaje);
-}
